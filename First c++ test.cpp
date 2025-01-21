ad9817cf2ef59c508e956059fa31c82e1fb2748c
Mathis Fortin <mathis14fortin@gmail.com>
2025-01-21 15:16:54 -0700

    first commit

---
+++ First c++ test.cpp
@@ -0,0 +1,16 @@
+// This is a comment
+// header import file
+#include <iostream>
+#include <math.h>
+#
+// standard I/O
+using namespace std;
+
+int main () {
+    // initialize a integer variable type
+    int testvar;
+    // assign a value to variable
+    testvar=12;
+    cout << testvar;
+    
+};
